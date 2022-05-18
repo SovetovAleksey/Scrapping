dc1072d6668fd314b1301eed1bc00e26e76067cd
SovetovAleksey <91490501+SovetovAleksey@users.noreply.github.com>
2022-05-18 18:11:55 +0300

    Add files via upload

---
+++ 1_quest.py
@@ -0,0 +1,28 @@
+import requests
+
+from pprint import pprint
+
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.141 YaBrowser/22.3.4.731 Yowser/2.5 Safari/537.36'}
+
+params = {'user_id': '228626486',
+          'access_token': 'df183d42445c31c3154f4255370193cf769c945d361e9fcb17181a8938d9b78b37ab076faa32759998a33',
+          'extended': '1'}
+
+url = 'https://api.vk.com/method/groups.get?user_id=228626486&v=5.131'
+response = requests.get(url, params=params, headers=headers)
+
+j_data = response.json()
+
+#pprint(j_data)
+groups = []
+
+for i in j_data['response']['items']:
+    groups.append(i['name'])
+
+print(groups, sep='\n')
+
+with open('HW1.html', 'w', encoding='UTF-8') as f:
+    f.write(response.text)
+
+
+
